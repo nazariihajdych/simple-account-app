940fc44befd8b9b478b352ef391444871e36f3db
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-04 22:47:33 +0200

    add web config and Dispatcher servlet initializer

---
+++ src/main/java/ua/ithillel/app/web/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package ua.ithillel.app.web;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        context.scan("ua.ithillel.app");
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("app", dispatcherServlet);
+
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+    }
+}

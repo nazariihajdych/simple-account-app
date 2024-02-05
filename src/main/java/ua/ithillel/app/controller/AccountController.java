f5f279ccd36781ee9516f67ad587a85f8e60d370
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-05 22:55:54 +0200

    resolve service call

--- src/main/java/ua/ithillel/app/controller/AccountController.java
+++ src/main/java/ua/ithillel/app/controller/AccountController.java
@@ -1,6 +1,5 @@
 package ua.ithillel.app.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,40 +11,47 @@ import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/account")
-@RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping( value = "/{id}")
-    public ResponseEntity<Account> getAccountById(@PathVariable Integer id){
-        if (accountService.getAccountById(id) == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        return ResponseEntity.ok(accountService.getAccountById(id));
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Integer id) {
+        Account accountById = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountById);
     }
 
     @GetMapping()
-    public ResponseEntity<List<Account>> getAllAccounts(){
+    public ResponseEntity<List<Account>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAllAccounts());
     }
 
     @PostMapping()
-    public ResponseEntity<Account> addAccount(@RequestBody Account account){
+    public ResponseEntity<Account> addAccount(@RequestBody Account account) {
         return ResponseEntity.ok(accountService.addAccount(account));
     }
 
-    @GetMapping( "/balance/{moreThen}")
-    public ResponseEntity<List<Account>> balanceMoreThen(@PathVariable Double moreThen){
-        if(accountService.balanceMoreThen(moreThen).isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        return ResponseEntity.ok(accountService.balanceMoreThen(moreThen));
+    @GetMapping("/balance/{moreThen}")
+    public ResponseEntity<List<Account>> balanceMoreThen(@PathVariable Double moreThen) {
+        List<Account> accountList = accountService.balanceMoreThen(moreThen);
+        if (accountList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else {
+            return ResponseEntity.ok(accountList);
+        }
     }
 
     @GetMapping("/countries")
-    public ResponseEntity<Set<String>> accountsCountries(){
+    public ResponseEntity<Set<String>> accountsCountries() {
         return ResponseEntity.ok(accountService.accountsCountries());
     }
 
     @GetMapping("/countries/average")
-    public ResponseEntity<Double> averageBalanceByCountries(@RequestParam String country){
+    public ResponseEntity<Double> averageBalanceByCountries(@RequestParam String country) {
         return ResponseEntity.ok(accountService.averageBalanceByCountry(country));
     }
 }

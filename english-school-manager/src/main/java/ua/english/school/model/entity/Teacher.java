f1ba1deff83da10e486e2dd878e12563a0aa0596
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-16 04:09:08 +0200

    create entities

---
+++ english-school-manager/src/main/java/ua/english/school/model/entity/Teacher.java
@@ -0,0 +1,23 @@
+package ua.english.school.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_info_id")
+    private UserInfo userInfo;
+
+    private String qualification;
+}
---
+++ english-school-manager/src/main/java/ua/english/school/model/entity/enums/HomeworkStatus.java
@@ -0,0 +1,8 @@
+package ua.english.school.model.entity.enums;
+
+public enum HomeworkStatus {
+    NOT_PROVIDED(),
+	OVERDUE(),
+	REVIEWING(),
+	GRADED()
+}

dc67fc474e60183dac98424ea18262483a4e2dc4
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-22 22:25:36 +0200

    refactor Account

--- account-app-common/src/main/java/ua/ithillel/app/model/Account.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/Account.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import ua.ithillel.app.model.enums.Gender;
 
 import java.util.Date;

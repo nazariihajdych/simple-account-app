b4122592fb369506e9e8ddbf2e93e8fbcd737c64
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-26 21:52:49 +0200

    refactor mappers

--- account-app-common/src/main/java/ua/ithillel/app/model/mapper/PaymentMapper.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/mapper/PaymentMapper.java
@@ -1,6 +1,7 @@
 package ua.ithillel.app.model.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import ua.ithillel.app.model.Payment;
 import ua.ithillel.app.model.dto.PaymentDTO;
@@ -8,6 +9,7 @@ import ua.ithillel.app.model.dto.PaymentDTO;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public abstract class PaymentMapper {
 
+    @Mapping(source = "account.id", target = "accountId")
     public abstract PaymentDTO paymentToPaymentDTO(Payment payment);
 
     public abstract Payment paymentDTOToPayment(PaymentDTO paymentDTO);
--- account-app-common/src/main/java/ua/ithillel/app/model/mapper/UserMapper.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/mapper/UserMapper.java
@@ -5,28 +5,51 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
+import ua.ithillel.app.model.Account;
 import ua.ithillel.app.model.Role;
 import ua.ithillel.app.model.User;
+import ua.ithillel.app.model.dto.AccountDTO;
 import ua.ithillel.app.model.dto.RoleDTO;
 import ua.ithillel.app.model.dto.UserDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        uses = {RolesMapper.class})
+        uses = {RolesMapper.class, AccountMapper.class})
 public abstract class UserMapper {
-//    @Autowired
-//    protected RolesMapper rolesMapper;
-//    @Mapping(source = "roles", target = "roles", qualifiedByName = "rolesToRolesDTO")
+    @Autowired
+    protected RolesMapper rolesMapper;
+    @Autowired
+    protected AccountMapper accountMapper;
+
+    @Mapping(source = "roles", target = "roles", qualifiedByName = "rolesToRolesDTO")
+    @Mapping(source = "accounts", target = "accounts", qualifiedByName = "accountToAccountDTO")
     public abstract UserDTO userToUserDTO(User user);
 
     public abstract User userDTOToUser(UserDTO userDTO);
 
-//    @Named("rolesToRolesDTO")
-//    public List<RoleDTO> rolesToRolesDTO(List<Role> roles) {
-//        return roles.stream()
-//                .map(rolesMapper::rolesToRolesDTO)
-//                .toList();
-//    }
+    @Named("rolesToRolesDTO")
+    public List<RoleDTO> rolesToRolesDTO(List<Role> roles) {
+        if (roles == null || roles.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return roles.stream()
+                .map(rolesMapper::rolesToRolesDTO)
+                .toList();
+    }
+
+    @Named("accountToAccountDTO")
+    public List<AccountDTO> accountToAccountDTO(List<Account> accounts) {
+        if (accounts == null || accounts.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return accounts.stream()
+                .map(accountMapper::accountToAccountDTO)
+                .toList();
+    }
+
 }

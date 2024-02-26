6f77ae57bbd39086a5491a972ba6d84208232a32
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-02-26 21:51:39 +0200

    change repo in AccountService

--- account-app-service/src/main/java/ua/ithillel/app/service/AccountService.java
+++ account-app-service/src/main/java/ua/ithillel/app/service/AccountService.java
@@ -1,16 +1,13 @@
 package ua.ithillel.app.service;
 
-import ua.ithillel.app.model.Account;
 import ua.ithillel.app.model.dto.AccountDTO;
 
 import java.util.List;
-import java.util.Set;
 
 public interface AccountService {
-    //CRUD
     AccountDTO addAccount(AccountDTO accountDTO);
     List<AccountDTO> getAllAccountsByUserId(Long id);
     AccountDTO getAccountById(Long id);
-    AccountDTO deleteAccount(Long id);
+    void deleteAccount(Long id);
     AccountDTO editAccount(Long id, AccountDTO accountDTO);
 }
--- account-app-service/src/main/java/ua/ithillel/app/service/AccountServiceImpl.java
+++ account-app-service/src/main/java/ua/ithillel/app/service/AccountServiceImpl.java
@@ -6,59 +6,85 @@ import ua.ithillel.app.exeption.UserNotFoundException;
 import ua.ithillel.app.model.Account;
 import ua.ithillel.app.model.User;
 import ua.ithillel.app.model.dto.AccountDTO;
-import ua.ithillel.app.model.enums.Gender;
 import ua.ithillel.app.model.mapper.AccountMapper;
+import ua.ithillel.app.model.mapper.PaymentMapper;
 import ua.ithillel.app.repo.AccountRepo;
 import ua.ithillel.app.repo.UserRepo;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    private final AccountRepo accountRepo;
     private final AccountMapper accountMapper;
     private final UserRepo userRepo;
+    private final AccountRepo accountRepo;
+    private final PaymentMapper paymentMapper;
 
-    public AccountServiceImpl(AccountRepo accountRepo, AccountMapper accountMapper, UserRepo userRepo) {
-        this.accountRepo = accountRepo;
+    public AccountServiceImpl(AccountMapper accountMapper, UserRepo userRepo, AccountRepo accountRepo, PaymentMapper paymentMapper) {
         this.accountMapper = accountMapper;
         this.userRepo = userRepo;
+        this.accountRepo = accountRepo;
+        this.paymentMapper = paymentMapper;
     }
 
     @Override
     public AccountDTO addAccount(AccountDTO accountDTO) {
         Account account = accountMapper.accountDTOToAccount(accountDTO);
-        User user = userRepo.findById(accountDTO.getUser_id())
-                .orElseThrow(() -> new UserNotFoundException("User with id = " + accountDTO.getUser_id() + "not found"));
+        User user = getUserOrThrow(accountDTO.getUser_id());
         account.setUser(user);
-        Account added = accountRepo.addAccount(account);
-        return accountMapper.accountToAccountDTO(added);
+        Account save = accountRepo.save(account);
+        return accountMapper.accountToAccountDTO(save);
     }
 
     @Override
     public List<AccountDTO> getAllAccountsByUserId(Long id) {
-        User user = userRepo.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User with id = " + id + "not found"));
+        User user = getUserOrThrow(id);
         return user.getAccounts().stream()
                 .map(accountMapper::accountToAccountDTO)
                 .toList();
     }
+
     @Override
     public AccountDTO getAccountById(Long id) {
-        return accountMapper.accountToAccountDTO(accountRepo.getAccountById(id));
+        Account account = getAccountOrThrow(id);
+        return accountMapper.accountToAccountDTO(account);
     }
 
     @Override
-    public AccountDTO deleteAccount(Long id) {
-        return accountMapper.accountToAccountDTO(accountRepo.deleteAccount(id));
+    public void deleteAccount(Long id) {
+        Account accountById = getAccountOrThrow(id);
+
+        User user = getUserOrThrow(accountById.getUser().getId());
+        user.getAccounts().removeIf(account -> account.getId().equals(id));
+        userRepo.save(user);
     }
 
     @Override
     public AccountDTO editAccount(Long id, AccountDTO accountDTO) {
-        Account account = accountMapper.accountDTOToAccount(accountDTO);
-        Account editedAccount = accountRepo.editAccount(id, account);
-        return accountMapper.accountToAccountDTO(editedAccount);
+        Account account = getAccountOrThrow(id);
+        account.setFirstName(accountDTO.getFirstName());
+        account.setLastName(accountDTO.getLastName());
+        account.setDateOfBirth(accountDTO.getDateOfBirth());
+        account.setCountry(accountDTO.getCountry());
+        account.setGender(accountDTO.getGender());
+        account.setPayments(accountDTO.getPayments().stream()
+                .map(paymentMapper::paymentDTOToPayment)
+                .toList());
+
+        User user = getUserOrThrow(accountDTO.getUser_id());
+        account.setUser(user);
+        Account save = accountRepo.save(account);
+        return accountMapper.accountToAccountDTO(save);
+    }
+
+    private User getUserOrThrow(Long id) {
+        return userRepo.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with id = " + id + "not found"));
+    }
+
+    private Account getAccountOrThrow(Long id) {
+        return accountRepo.findById(id)
+                .orElseThrow(() -> new AccountNotFoundException("Account with id = " + id + "not found"));
     }
 }

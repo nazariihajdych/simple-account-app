01765944f302cfe98a28c2ed000f20c08f2cccd2
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-03-02 15:06:02 +0200

    add validation for UserDTO fields

--- account-app-common/src/main/java/ua/ithillel/app/model/dto/UserDTO.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/dto/UserDTO.java
@@ -2,7 +2,11 @@ package ua.ithillel.app.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
 
 import java.util.Date;
 import java.util.List;
@@ -12,6 +16,9 @@ public class UserDTO {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private Date createdDate;
+    @NotNull
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
+    message = "not valid email")
     private String email;
     @JsonIgnore
     private String password;

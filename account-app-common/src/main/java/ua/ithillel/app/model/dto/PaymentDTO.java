e9c94d58baa896383bd3199322a78ec19f8c2c69
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2024-03-02 15:06:37 +0200

    add validation for PaymentDTO fields

--- account-app-common/src/main/java/ua/ithillel/app/model/dto/PaymentDTO.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/dto/PaymentDTO.java
@@ -1,10 +1,16 @@
 package ua.ithillel.app.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class PaymentDTO {
     private Long id;
+    @PositiveOrZero(message = "should be positive number or zero")
     private Double amount;
+    @NotNull(message = "should not be null")
+    @Positive(message = "should be positive number")
     private Long accountId;
 }

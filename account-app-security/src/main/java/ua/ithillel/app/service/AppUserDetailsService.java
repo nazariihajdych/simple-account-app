c86f060361c468fc332c3eb16ade9a568300350e
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-08 23:09:00 +0200

    add UserDetailsImpl and UserDetailsService

---
+++ account-app-security/src/main/java/ua/ithillel/app/service/AppUserDetailsService.java
@@ -0,0 +1,24 @@
+package ua.ithillel.app.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ua.ithillel.app.model.User;
+import ua.ithillel.app.repo.UserRepo;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    public AppUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findUserByEmail(email);
+        return new UserDetailsImpl(user);
+    }
+}
---
+++ account-app-security/src/main/java/ua/ithillel/app/service/UserDetailsImpl.java
@@ -0,0 +1,56 @@
+package ua.ithillel.app.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ua.ithillel.app.model.User;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

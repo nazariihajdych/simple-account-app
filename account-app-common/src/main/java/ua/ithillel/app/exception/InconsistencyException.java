ec0ddc19d1cfd978cb38450b173e7225410f8565
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-10 01:08:19 +0200

    add InconsistencyException

---
+++ account-app-common/src/main/java/ua/ithillel/app/exception/InconsistencyException.java
@@ -0,0 +1,7 @@
+package ua.ithillel.app.exception;
+
+public class InconsistencyException extends RuntimeException {
+    public InconsistencyException(String message) {
+        super(message);
+    }
+}

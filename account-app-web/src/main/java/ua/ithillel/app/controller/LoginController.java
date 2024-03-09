1de01d51d46249c3d4775d7de2b0b32f7b8f027c
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-10 01:09:09 +0200

    add Login and Register controllers

---
+++ account-app-web/src/main/java/ua/ithillel/app/controller/LoginController.java
@@ -0,0 +1,27 @@
+package ua.ithillel.app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.ithillel.app.model.dto.AuthDTO;
+import ua.ithillel.app.model.dto.LoginRegisterDTO;
+import ua.ithillel.app.service.AuthService;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+    private final AuthService authService;
+
+    public LoginController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthDTO> login(@RequestBody LoginRegisterDTO loginRegisterDTO) throws UsernameNotFoundException {
+        AuthDTO authenticate = authService.authenticate(loginRegisterDTO);
+        return ResponseEntity.ok(authenticate);
+    }
+}

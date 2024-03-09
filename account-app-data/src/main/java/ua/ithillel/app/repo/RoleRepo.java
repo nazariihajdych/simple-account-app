e5904ebd08995955138898fb829527dac6d23d01
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-10 01:06:38 +0200

    add method to find role by name

--- account-app-data/src/main/java/ua/ithillel/app/repo/RoleRepo.java
+++ account-app-data/src/main/java/ua/ithillel/app/repo/RoleRepo.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.ithillel.app.model.Role;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByRoleName(String roleName);
 }

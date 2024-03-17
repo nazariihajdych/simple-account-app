5b86dfc39b751209f61b452c8c2c6fe309e31cc0
nazariihajdych <wertyp.poiteq@gmail.com>
2024-03-17 23:53:38 +0200

    add table name annotation

--- account-app-common/src/main/java/ua/ithillel/app/model/User.java
+++ account-app-common/src/main/java/ua/ithillel/app/model/User.java
@@ -1,18 +1,6 @@
 package ua.ithillel.app.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(exclude = {"roles", "accounts"})
+@Table(name = "t_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
